585959fd4fb3e403f594353a1f5f6a488b4a0cff
Kvkthecreator <134989386+Kvkthecreator@users.noreply.github.com>
2025-06-07 13:48:58 +0900

    Use font-brand for yarnnn branding

--- web/components/landing/LandingHeader.tsx
+++ web/components/landing/LandingHeader.tsx
@@ -5,7 +5,7 @@ export default function LandingHeader() {
   return (
     <header className="w-full py-4 px-5 flex justify-between items-center bg-white">
       <div className="text-xl">
-        <Link href="/" className="text-black hover:text-black/80 transition-colors font-pacifico">
+        <Link href="/" className="text-black hover:text-black/80 transition-colors font-brand">
           yarnnn
         </Link>
       </div>
