bb29b54fb8c3189327695f0ed8d4053c4a6153cf
Kvkthecreator <134989386+Kvkthecreator@users.noreply.github.com>
2025-07-15 01:35:31 -0700

    refactor: remove shell wrapper from basket layout

--- web/app/baskets/[id]/layout.tsx
+++ web/app/baskets/[id]/layout.tsx
@@ -1,5 +1,4 @@
 import { BasketProvider } from "@/lib/context/BasketContext";
-import Shell from "@/components/layout/Shell";
 
 export default async function BasketLayout({
   children,
@@ -10,8 +9,6 @@ export default async function BasketLayout({
 }) {
   const { id } = await params;
   return (
-    <Shell>
-      <BasketProvider initialBasketId={id}>{children}</BasketProvider>
-    </Shell>
+    <BasketProvider initialBasketId={id}>{children}</BasketProvider>
   );
 }
